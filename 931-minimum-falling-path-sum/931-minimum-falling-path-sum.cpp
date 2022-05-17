ecfc32887acc7372a78e95f4c0d7f88834e8b8d4
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2022-05-17 22:05:56 +0530

    Time: 17 ms (63.00%), Space: 9.8 MB (75.51%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,42 +1,9 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int res = 1e5;
-        for(int i = 0 ; i < n ; i++){
-            res = min(res , path(matrix , i));
-            // res = min(res , temp);
-            // cout << "\n";
-        }    
-        return res;
-    }
-    
-    int path(vector<vector<int>> ms , int x){
-        int start = x , end = x;
-        int n = ms.size();
-        vector<vector<int>> matrix(n , vector<int>(n , 1e5));
-        int level = 0;
-        for(int i = 0; i < n ; i++) matrix[0][i] = ms[0][i];
-        while( level < n - 1 ){
-            for(int i = start ; i <= end ; i++){
-                if( i > 0 ) matrix[level + 1][i - 1] = min(matrix[level + 1][i - 1] , ms[level + 1][i - 1] + matrix[level][i] );
-                matrix[level + 1][i] = min(matrix[level + 1][i] , ms[level + 1][i] + matrix[level][i]);
-                if( i < n - 1) matrix[level + 1][i + 1] = min(matrix[level + 1][i + 1] , ms[level + 1][i + 1] + matrix[level][i]);
-            }
-            if( start > 0 ) start--;
-            if( end < n - 1) end++;
-            level += 1;
-        }
-        int res = 1e5;
-        // for(auto i : matrix){
-        //     for(auto j : i){
-        //         cout << j << " ";
-        //     }
-        //     cout << "\n";
-        // }
-        for(int i = 0; i < n ; i++){
-            res = min(res , matrix[n - 1][i]);
-        }
-        return res;
+    int minFallingPathSum(vector<vector<int>>& A) {
+      for (auto i = 1; i < A.size(); ++i)
+        for (auto j = 0; j < A.size(); ++j)
+          A[i][j] += min({ A[i-1][j], A[i-1][max(0,j-1)], A[i-1][min((int)A.size()-1,j+1)] });
+      return *min_element(begin(A[A.size() - 1]), end(A[A.size() - 1]));
     }
 };
