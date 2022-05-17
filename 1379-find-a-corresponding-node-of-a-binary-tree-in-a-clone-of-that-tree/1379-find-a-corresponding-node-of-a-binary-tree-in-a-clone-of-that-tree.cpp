2de15e6c9d971905eb6b00caeca3bea088c5ef05
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2022-05-17 08:55:20 +0530

    Time: 1001 ms (6.28%), Space: 163.8 MB (95.67%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -11,32 +11,10 @@
 class Solution {
 public:
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        vector<char> res;
-        vector<char> dir;
-        dfs(original , target , dir , res);
-        for(auto& i : res){
-            if( i == 'l') cloned = cloned->left;
-            else cloned = cloned->right;
-        }
-        return cloned;
-        
-    }
-    void dfs(TreeNode* o , TreeNode* t , vector<char>& d , vector<char>& res){
-        if(!o) return;
-        if( o == t ){
-            res = d;
-            return;
-        }
-        if(o->left && res.empty()){
-            d.push_back('l');
-            dfs(o->left , t , d , res);
-            d.pop_back();
-        }
-        if(o->right && res.empty()){
-            d.push_back('r');
-            dfs(o->right , t , d , res);
-            d.pop_back();
-        }
-        return;
+        if(original == NULL) return NULL;
+        if(original == target) return cloned;
+        TreeNode* l = getTargetCopy(original->left, cloned->left, target);
+        TreeNode* r = getTargetCopy(original->right, cloned->right, target);
+        return (l!=NULL) ?  l :  r;
     }
 };
