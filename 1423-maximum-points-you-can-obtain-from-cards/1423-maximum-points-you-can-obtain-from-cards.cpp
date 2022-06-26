bc5cefd182714db4f473f57c4028f6df4f5a0831
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2022-06-27 00:44:54 +0530

    Time: 81 ms (60.75%), Space: 42.4 MB (48.14%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -4,7 +4,7 @@ public:
         int total = 0;
         for (int i = 0; i < K; i++) total += C[i];
         int best = total;
-        for (int i = K - 1, j = C.size() - 1; ~i; i--, j--)
+        for (int i = K - 1, j = C.size() - 1; i >= 0; i--, j--)
             total += C[j] - C[i], best = max(best, total);
         return best;
     }
