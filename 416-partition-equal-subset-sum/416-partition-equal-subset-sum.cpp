dcd58b984bd11a45494f74385554a013b4d32773
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2022-07-13 21:22:20 +0530

    Time: 909 ms (5.77%), Space: 377.6 MB (5.05%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
+    int dp[20001][201];
     bool canPartition(vector<int>& nums) {
-        int sum = 0 , n = nums.size();
-        for(int& i : nums) sum += i;
-        if( sum % 2 == 1 ) return false;
+        memset(dp , -1 , sizeof(dp));
+        int n = nums.size() , sum;
+        sum = accumulate(nums.begin() , nums.end() , 0);
+        if( sum % 2 != 0) return false;
         int target = sum / 2;
+        return combos(nums , target , 0);
+    }
+    
+    int combos(vector<int> nums , int target , int x){
+        if( target == 0 ) return 1;
+        if( x >= nums.size()) return 0;
         
-        vector<vector<bool>> dp( n + 1, vector<bool>(target + 1 , false));
+        if( dp[target][x] != -1)
+            return dp[target][x];
         
-        for(int i = 0; i <= n ; i++){
-            for(int j = 0; j <= target ; j++){
-                if( i == 0 || j == 0) 
-                    dp[i][j] = false;
-                else if ( nums[i - 1] > j)
-                    dp[i][j] = dp[i - 1][j];
-                else if (nums[i - 1] == j)
-                    dp[i][j] = true;
-                else
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
-            }
-        }
+        if( nums[x] <= target )
+            dp[target][x] = combos(nums , target - nums[x] , x + 1) || combos(nums , target , x + 1);
+        else
+            dp[target][x] = combos(nums , target , x + 1);
         
-        return dp[n][target];
+        return dp[target][x];
     }
 };
