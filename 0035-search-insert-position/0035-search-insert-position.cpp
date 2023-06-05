02761d718be8023d12829bfeb4e836e5feeb570c
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2023-06-05 08:56:07 +0530

    Time: 3 ms (88.98%), Space: 9.6 MB (63.82%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,6 +3,6 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         auto itr = lower_bound( nums.begin() , nums.end() , target);
         
-        return int(itr - nums.begin());
+        return (itr - nums.begin());
     }
 };
