e5799c67e9b153d76d5c3023ba09fd3508a1a7db
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2024-05-25 13:01:53 +0530

    Time: 2 ms (69.68%), Space: 2.6 MB (13.86%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.go
+++ 0014-longest-common-prefix/0014-longest-common-prefix.go
@@ -1,24 +1,34 @@
 func longestCommonPrefix(strs []string) string {
-    res := strs[0]
-    
-    for _ , elem := range strs {
-        res = lcs(res , elem)
-    }
-    
-    return res
-}
-
-func lcs(str1 string , str2 string ) string {
-    i := 0
-    
-    for i < len(str1) && i < len(str2) && str1[i] == str2[i] {
-        i++
-    }
-    
-    if i < 0 {
+    if len(strs) == 0 {
         return ""
     }
-    
+
+    prefix := strs[0]
+
+    for _, str := range strs[1:] {
+        prefix = commonPrefix(prefix, str)
+        if prefix == "" {
+            break
+        }
+    }
+
+    return prefix
+}
+
+func commonPrefix(str1, str2 string) string {
+    minLength := min(len(str1), len(str2))
+
+    i := 0
+    for i < minLength && str1[i] == str2[i] {
+        i++
+    }
+
     return str1[:i]
-    
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
 }
