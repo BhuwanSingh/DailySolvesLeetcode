44aa2fb91f05822e28f443ab65f6ad9e26332f4a
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2024-07-11 19:12:34 +0530

    Time: 22 ms (67.91%), Space: 16.8 MB (44.94%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.swift
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.swift
@@ -1,14 +1,10 @@
 class Solution {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
-        if nums.count == 0 {
-            return 0
-        }
-        if nums.count == 1 {
-            return 1
-        }
+        guard !nums.isEmpty else { return 0 }
+        
         var pointer = 0
-        for i in 1..<nums.count{
-            if nums[pointer] != nums[i]{
+        for i in 1..<nums.count {
+            if nums[pointer] != nums[i] {
                 pointer += 1
                 nums[pointer] = nums[i]
             }
