0d90df40381ce317faa626292256e0a9abb66642
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2024-07-16 19:31:07 +0530

    Time: 554 ms (42.86%), Space: 34.2 MB (42.86%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.swift
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.swift
@@ -14,30 +14,31 @@
  * }
  */
 class Solution {
-    func find(_ node: TreeNode?, _ val: Int, _ path: inout String) -> Bool {
-        guard let node = node else { return false }
+    func find( _ node: TreeNode? , _ val : Int , _ path : inout String ) -> Bool {
+        guard let node = node else { return false}
         if node.val == val { return true }
-
-        if let left = node.left, find(left, val, &path) {
+        
+        if let left = node.left , find(left , val , &path) {
             path.append("L")
-        } else if let right = node.right, find(right, val, &path) {
+        } else if let right = node.right , find(right , val , &path){
             path.append("R")
         }
+        
         return !path.isEmpty
     }
-
+    
     func getDirections(_ root: TreeNode?, _ startValue: Int, _ destValue: Int) -> String {
         var sPath = ""
         var dPath = ""
-
-        _ = find(root, startValue, &sPath)
-        _ = find(root, destValue, &dPath)
-
+        
+        _ = find(root , startValue , &sPath)
+        _ = find(root , destValue , &dPath)
+        
         while !sPath.isEmpty && !dPath.isEmpty && sPath.last == dPath.last {
             sPath.removeLast()
             dPath.removeLast()
         }
-
-        return String(repeating: "U", count: sPath.count) + String(dPath.reversed())
+        
+        return String(repeating: "U" , count: sPath.count) + String(dPath.reversed())
     }
 }
