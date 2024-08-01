90868bacceb92665935b68056fb3cb3f6721c767
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2024-08-01 09:49:50 +0530

    Time: 22 ms (68.97%), Space: 16.5 MB (41.38%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.swift
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.swift
@@ -1,14 +1,5 @@
 class Solution {
     func countSeniors(_ details: [String]) -> Int {
-        var ans: Int = 0
-        for info in details {
-            let startIndex = info.index(info.startIndex, offsetBy: 11)
-            let endIndex = info.index(info.startIndex, offsetBy: 12)
-            let ageStr = String(info[startIndex...endIndex])
-            if let Age = Int(ageStr), Age > 60 {
-                ans += 1
-            }
-        }
-        return ans
+        details.filter { Int($0.dropFirst(11).prefix(2))! > 60 }.count
     }
 }
