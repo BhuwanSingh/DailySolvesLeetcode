754e404b54c27a944afee344573334f058367bc5
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2024-08-11 12:14:16 +0530

    Time: 17 ms (90.14%), Space: 57.9 MB (32.67%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -4,7 +4,7 @@ class Solution {
         
         for(int i = 0; i < nums.length ; i++){
             if( hs.containsKey(nums[i]) ){
-                int itr = hs.get(nums[i]);
+                Integer itr = hs.get(nums[i]);
                 if( i - itr <= k) {
                     return true;
                 }
