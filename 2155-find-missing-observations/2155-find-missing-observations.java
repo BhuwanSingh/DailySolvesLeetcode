c86cac19c0ee383b2966a195f706dbe1d88a7031
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2024-09-05 22:18:31 +0530

    Time: 3 ms (98.03%), Space: 60.2 MB (60.98%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.java
+++ 2155-find-missing-observations/2155-find-missing-observations.java
@@ -1,14 +1,25 @@
-class Solution {
-    public int[] missingRolls(int[] rolls, int mean, int n) {
-        int curSum = Arrays.stream(rolls).sum(), m = rolls.length;
-        int missingSum = mean * (n + m) - curSum;
-        if (missingSum < n || missingSum > 6*n) return new int[0];
 
-        int part = missingSum / n, rem = missingSum % n;
-        int[] ans = new int[n];
-        Arrays.fill(ans, part);
-        for (int i = 0; i < rem; ++i)
-            ++ans[i];
-        return ans;
+
+class Solution {
+
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        int[] ret = new int[n];
+        int sum = 0;
+        for (int i : rolls) {
+            sum += i;
+        }
+        int missingSum = mean * (n + rolls.length) - sum;
+        if (missingSum > 6*n || missingSum < n) {
+            return new int[]{};
+        }
+        int expectedAvg = missingSum/n, remainder = missingSum%n;
+
+        for (int i = 0; i < remainder; i++) {
+            ret[i] = expectedAvg + 1;
+        }
+        for (int i = remainder; i < n;i++) {
+            ret[i] = expectedAvg;
+        }
+        return ret;
     }
 }
