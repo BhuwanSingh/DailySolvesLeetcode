f4a8ef1d4ec3ea9c3742b735c97112bd81f878cc
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2024-12-04 20:36:37 +0530

    Time: 28 ms (38.9%), Space: 9.7 MB (38.92%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -1,33 +1,29 @@
 func threeSum(nums []int) [][]int {
+    var results [][]int
     sort.Ints(nums)
-    n := len(nums)
-    
-    var res [][]int
-    
-    for i := 0 ; i < n - 2 ; i++ {
-        if i > 0 && nums[i] == nums[i - 1]{
+    for i := 0 ; i < len(nums) - 2 ; i++ {
+        if i > 0 && nums[i] == nums[ i - 1 ]{
             continue
         }
-        l , r := i + 1 , n - 1
-        for l < r {
-            sum := nums[i] + nums[l] + nums[r]
-            if sum  < 0 {
-                l++
-            } else if sum > 0 {
-                r--
+        target , left , right := -nums[i] , i + 1 , len(nums) - 1;
+        for left < right {
+            sum := nums[left] + nums[right]
+            if sum == target {
+                results = append(results , []int{nums[i] , nums[left],  nums[right]})
+                left++
+                right--
+                for left < right && nums[left] == nums[left - 1]{
+                    left++
+                }
+                for left < right && nums[right] == nums[right + 1]{
+                    right--
+                }
+            } else if sum > target {
+                right--
             } else {
-                res = append( res , []int{nums[i] , nums[l] , nums[r]})  
-                for l < r && nums[l] == nums[l + 1] {
-                    l++
-                }
-                for l < r && nums[r] == nums[r - 1] {
-                    r--
-                }
-                l++
-                r--
+                left++
             }
         }
     }
-    
-    return res 
+    return results
 }
