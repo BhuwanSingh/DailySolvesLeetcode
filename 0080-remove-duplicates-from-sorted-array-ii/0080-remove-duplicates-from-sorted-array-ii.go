614c1698d9a63204b0694e8cce1092001090564d
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-01-10 19:43:51 +0530

    Time: 5 ms (31.02%), Space: 4.7 MB (63.8%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.go
@@ -0,0 +1,16 @@
+func removeDuplicates(nums []int) int {
+    index := 1
+    occurance := 1
+    for i := 1; i < len(nums) ; i++ {
+        if nums[i] == nums[i - 1 ]{
+            occurance++
+        } else {
+            occurance = 1
+        }
+        if occurance <= 2 {
+            nums[index] = nums[i]
+            index++
+        }
+    }
+    return index
+}
