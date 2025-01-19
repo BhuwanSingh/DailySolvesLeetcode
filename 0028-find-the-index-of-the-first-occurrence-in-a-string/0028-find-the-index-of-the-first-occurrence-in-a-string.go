9b4a4a03cc506d8f1b45909edc6d557f3b276bfd
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-01-19 13:22:37 +0530

    Time: 0 ms (100%), Space: 4 MB (7.91%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.go
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.go
@@ -1,3 +1,27 @@
 func strStr(haystack string, needle string) int {
-    return strings.Index(haystack , needle )
+    h := len(haystack)
+    n := len(needle)
+    switch {
+        case n == 0:
+            return 0
+        case n == 1:
+            for i := 0 ; i < h ; i++ {
+                if string(haystack[i]) == needle {
+                    return i
+                }
+            }
+        case n == h:
+            if haystack == needle {
+                return 0
+            }
+        case n > h:
+            return -1
+        default:
+            for i := 0 ; i < h - n + 1 ; i++ {
+                if haystack[i : i + n] == needle {
+                    return i
+                }
+            }
+    }
+    return -1
 }
