d10562ad4157a7c3b6a4fc2f63388793b94f783b
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-01-19 13:59:49 +0530

    Time: 0 ms (100%), Space: 4.8 MB (62.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.go
+++ 0125-valid-palindrome/0125-valid-palindrome.go
@@ -1,18 +1,23 @@
-func isPalindrome(s string) bool {
-    left, right := 0 , len(s) - 1
+import "unicode"
 
-    for left < right {
-        for left < len(s) && (!unicode.IsLetter(rune(s[left])) && !unicode.IsNumber(rune(s[left]))) {
-            left++
+func isPalindrome(s string) bool {
+    f := func(r rune) rune {
+        if !unicode.IsLetter(r) && !unicode.IsNumber(r) {
+            return -1
         }
-        for right >= 0 && (!unicode.IsLetter(rune(s[right])) && !unicode.IsNumber(rune(s[right]))) {
-            right--
-        }
-        if left < len(s) && right >= 0 &&  unicode.ToLower(rune(s[left])) != unicode.ToLower(rune(s[right])) {
+        return unicode.ToLower(r)
+    }
+    
+    s = strings.Map(f , s)
+
+    i , j := 0 , len(s) - 1 
+
+    for i < j {
+        if s[i] != s[j] {
             return false
         }
-        left++
-        right--
+        i = i + 1
+        j = j - 1
     }
     return true
 }
