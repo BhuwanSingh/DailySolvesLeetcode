697585d548ff520cd6636c140397884eb71cd143
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-01-23 10:28:02 +0530

    Time: 2 ms (17.84%), Space: 9.7 MB (49.5%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.go
+++ 0011-container-with-most-water/0011-container-with-most-water.go
@@ -1,7 +1,7 @@
 func maxArea(height []int) int {
     i , j := 0 , len(height) - 1
     res := 0
-    for ; i <= j; {
+    for i <= j {
         if( height[i] < height[j] ){
             res = int( max(float64(res) , float64(height[i] * ( j - i ) )))
             i++
