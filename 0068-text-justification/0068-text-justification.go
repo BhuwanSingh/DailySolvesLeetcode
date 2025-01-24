61081b431b4ed9aa17ae9b9be002ea1a7eb5d82b
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-01-24 12:25:52 +0530

    Time: 0 ms (100%), Space: 4.1 MB (52.58%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.go
@@ -0,0 +1,39 @@
+func fullJustify(words []string, maxWidth int) []string {
+    justify := []string{}
+    current, curLength := []string{} , 0
+    for i , w := range words {
+        if curLength + len(current) + len(w) > maxWidth {
+            if len(current) == 1 {
+                curLine := current[0] + strings.Repeat(" ", maxWidth - len(current[0]))
+                justify = append(justify , curLine)
+            } else {
+                diff := maxWidth - curLength
+                spaces := diff / (len(current) - 1)
+                more := diff % (len(current) - 1)
+                curLine := bytes.Buffer{}
+                for ci , cw := range current {
+                    curLine.WriteString(cw) 
+                    if ci != len(current) - 1 {
+                        moreBlanks := 0
+                        if more > 0 {
+                            moreBlanks = 1
+                            more--
+                        }
+                        curLine.WriteString(strings.Repeat(" ", spaces + moreBlanks))
+                    }
+                }
+                justify = append(justify , curLine.String())
+            }
+            current , curLength = []string{} , 0
+        }
+        curLength += len(w)
+        current = append(current , w)
+
+        if i == len(words) - 1 {
+			lastLine := strings.Join(current, " ")
+			lastLine = lastLine + strings.Repeat(" ", maxWidth-len(lastLine))
+			justify = append(justify, lastLine)
+		}
+    }
+    return justify
+}
