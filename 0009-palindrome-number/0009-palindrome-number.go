2b3abde0258e44889808db5195562eb45bdd6484
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-01-26 17:04:13 +0530

    Time: 4 ms (50.7%), Space: 6 MB (77.88%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.go
+++ 0009-palindrome-number/0009-palindrome-number.go
@@ -1,29 +1,12 @@
-func isPalindrome(x int) bool {
-    if x < 0 {
+func isPalindrome(num int) bool {
+    if num < 0 {
         return false
     }
-    
-    temp := conv(x)
-    
-    l , r := 0 , len(temp) - 1
-    
-    for l < r {
-        // fmt.Print( temp[l] , " " , temp[r])
-        if( temp[l] != temp[r] ){
-            return false
-        }
-        l++
-        r--
-    }
-    return true
-}
-
-func conv(x int) string {
-    res := ""
-    for x > 0{
-        tmp := x % 10
+    x := num
+    reversed := 0
+    for x != 0 {
+        reversed = 10*reversed + x%10
         x /= 10
-        res += string(rune(tmp + 48))
     }
-    return res
+    return (reversed == num)
 }
