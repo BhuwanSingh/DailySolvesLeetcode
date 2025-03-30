297d24a391fc0dfb8fff37e3040350fa798e93b6
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-03-30 18:12:03 +0530

    Time: 4 ms (53.06%), Space: 4.4 MB (66.76%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -1,28 +1,37 @@
 func longestPalindrome(s string) string {
-    l , r , n := 0 , 0 , len(s)
-    var res string = ""
+    result := ""
 
-    for i := 0 ; i < n ; i++ {
-        l = i
-        r = i
-        for l >= 0 && r < n && s[l] == s[r] {
-            if( r - l + 1 > len(res)) {
-                res = s[l : r + 1]
+    for i := 0 ; i < len(s) ; i++ {
+        left := i
+        right := i
+        for left >= 0 && right < len(s) {
+            if s[left] == s[right] {
+                if len(s[left:right + 1]) > len(result){
+                    result = s[left:right + 1]
+                }
+            } else {
+                break
             }
-            l--;
-            r++;
+            left--
+            right++
         }
     }
-    for i := 0 ; i < n ; i++ {
-        l = i
-        r = i + 1
-        for l >= 0 && r < n  && s[l] == s[r] {
-            if( r - l + 1 > len(res)) {
-                res = s[l : r + 1]
+
+    for i := 1 ; i < len(s) ; i++ {
+        left := i - 1
+        right := i
+        for left >= 0 && right < len(s) {
+            if s[left] == s[right] {
+                if len(s[left:right + 1]) > len(result){
+                    result = s[left:right + 1]
+                }
+            } else {
+                break
             }
-            l--;
-            r++;
+            left--
+            right++
         }
     }
-    return res;
+
+    return result
 }
