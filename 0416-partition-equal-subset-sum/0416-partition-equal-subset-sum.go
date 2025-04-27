0f3a2094fa3d63ae7e1b779b252b8a033b404e4b
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-04-27 12:00:17 +0530

    Time: 13 ms (57.26%), Space: 4.5 MB (72.29%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.go
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.go
@@ -1,40 +1,21 @@
 func canPartition(nums []int) bool {
-    target := 0
-    for _, val := range nums {
-        target += val
+    sum := 0
+    for _, num := range nums {
+        sum += num
     }
-    if target%2 != 0 {
+    if sum%2 != 0 {
         return false
     }
-    target /= 2
 
-    dp := make([][]int, target+1)
-    for i := range dp {
-        dp[i] = make([]int, len(nums)+1)
+    target := sum / 2
+    dp := make([]bool, target+1)
+    dp[0] = true
+
+    for _, num := range nums {
+        for j := target; j >= num; j-- {
+            dp[j] = dp[j] || dp[j-num]
+        }
     }
 
-    return check(nums, target, 0, dp)
-}
-
-func check(nums []int, target, curr int, dp [][]int) bool {
-    if target == 0 {
-        return true
-    }
-    if curr == len(nums) || target < 0 {
-        return false
-    }
-    if dp[target][curr] != 0 {
-        return dp[target][curr] == 1
-    }
-
-    include := check(nums, target-nums[curr], curr+1, dp)
-    exclude := check(nums, target, curr+1, dp)
-
-    if include || exclude {
-        dp[target][curr] = 1
-        return true
-    }
-    
-    dp[target][curr] = -1
-    return false
+    return dp[target]
 }
