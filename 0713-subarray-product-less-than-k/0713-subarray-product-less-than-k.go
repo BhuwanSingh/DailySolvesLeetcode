1ff7a723fe36f1ffa7500d14766e066de479fa97
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-06-07 11:12:02 +0530

    Time: 2 ms (50.83%), Space: 9.1 MB (44.75%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.go
@@ -0,0 +1,13 @@
+func numSubarrayProductLessThanK(nums []int, k int) int {
+    var l, result int
+    curr := 1
+    for r := 0 ; r < len(nums) ; r++ {
+        curr *= nums[r]
+        for l <= r && curr >= k {
+            curr /= nums[l]
+            l++
+        }
+        result += r - l + 1
+    }
+    return result
+}
