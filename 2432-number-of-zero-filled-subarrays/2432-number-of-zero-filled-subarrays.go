6561644d0b96bb75ef1bf4c2a08e6a9c55ae0796
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-08-19 22:04:25 +0530

    Time: 2 ms (30%), Space: 10.5 MB (50%) - LeetHub

---
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.go
@@ -0,0 +1,12 @@
+func zeroFilledSubarray(nums []int) int64 {
+    var cnt, streak int64 = 0 , 0
+    for _, num := range nums {
+        if num == 0 {
+            streak++
+            cnt += streak
+        } else {
+            streak = 0
+        }
+    }
+    return cnt
+}
