3415af2ee49798a268a31b9b409f9566b9abdd54
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-10-27 20:06:32 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (38.64%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.go
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.go
@@ -1,24 +1,17 @@
 func numberOfBeams(bank []string) int {
-    res := []int{}
-    for _, val := range bank {
-        num := cal(val)
-        if num != 0{
-            res = append(res, num)
-        }
-    }
-    result := 0
-    for i := 1 ; i < len(res) ; i++ {
-        result += res[i] * res[i - 1]
-    }
-    return result
-}
+	totalBeams := 0
+	prevRowDeviceCount := 0
 
-func cal(str string) int {
-    res := 0
-    for _, val := range str {
-        if val == '1' {
-            res++
-        }
-    }
-    return res
+	for _, row := range bank {
+		currentRowDeviceCount := strings.Count(row, "1")
+
+		if currentRowDeviceCount > 0 {
+
+			totalBeams += prevRowDeviceCount * currentRowDeviceCount
+
+			prevRowDeviceCount = currentRowDeviceCount
+		}
+	}
+
+	return totalBeams
 }
