727a3463e8fbf1672e08c266e30ed190658a6c98
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-10-29 21:39:42 +0530

    Time: 0 ms (100.00%) | Memory: 4 MB (70.83%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.go
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.go
@@ -1,12 +1,6 @@
 func smallestNumber(n int) int {
-    var pow, res float64 = 0 , 0
-    temp := float64(n)
-    for res < temp {
-        res = math.Pow( float64(2), pow) - 1
-        if res >= temp {
-            return int(res)
-        }
-        pow++
-    }
-    return -1
+	for n & (n + 1) != 0 {
+		n |= n + 1
+	}
+	return n
 }
